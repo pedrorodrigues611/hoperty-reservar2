d235ad94feccfa0b90a39bc300c66532a0ba27f1
pedrorodrigues611 <hopertybnb@gmail.com>
2025-04-17 11:39:06 +0100

    Update Reservas.jsx

--- src/pages/Reservas.jsx
+++ src/pages/Reservas.jsx
@@ -28,7 +28,7 @@ export default function Reservas() {
     <div className='min-h-screen bg-gray-100 p-6'>
       <div className='max-w-7xl mx-auto'>
         <SearchBar initialParams={params} />
-        <h2 className='text-3xl font-bold mt-10 mb-6'>Resultados</h2>
+        <h2 className='text-3xl font-bold mt-10 mb-6'>Resultados1</h2>
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
           {properties.map((p) => <PropertyCard key={p.propertyId} data={p} />)}
         </div>
